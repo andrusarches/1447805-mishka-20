4799e7b4a1fbfead1cf1f8ec9c3d59705cd98744
andrusarches <64173881+andrusarches@users.noreply.github.com>
2020-09-01 23:29:10 -0400

    Завершение

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,17 @@ const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
 const csso = require("gulp-csso");
+const imagemin = require("gulp-imagemin");
+const optipng = require("imagemin-optipng");
+const svgo = require("imagemin-svgo");
+const mozjpeg = require("imagemin-mozjpeg");
+const webp = require("gulp-webp");
+const rename = require("gulp-rename");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+const terser = require("gulp-terser");
+const posthtml = require("gulp-posthtml");
+const posthtmlIinclude = require("posthtml-include");
 
 // Styles
 
@@ -17,19 +28,39 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename({
+      basename: "style",
+      suffix: ".min",
+      extname: ".css"
+    }))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// JS Compress
+
+const scripts = () => {
+  return gulp.src("source/js/script.js")
+  .pipe(terser())
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest("build/js"))
+  .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
@@ -44,9 +75,99 @@ exports.server = server;
 
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/*.html", gulp.series("html")).on("change", sync.reload);
 }
 
+// Imagemin
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+  .pipe(imagemin([
+    imagemin.mozjpeg({quality: 90, progressive: true}),
+    imagemin.optipng({optimizationLevel: 7}),
+    imagemin.svgo({
+        plugins: [
+            {removeViewBox: true},
+            {cleanupIDs: false}
+          ]
+      })
+  ]))
+  // .pipe(gulp.dest("dist/images"));
+}
+
+exports.images = images;
+
+// createWebp
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+          .pipe(webp({quality: 90}))
+          .pipe(gulp.dest("build/img"))
+}
+
+exports.createWebp = createWebp;
+
+// SvgStore - Spritesheet
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.sprite = sprite;
+
+// Copy to /build/
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+}
+
+exports.copy = copy;
+
+// Clean /build/
+
+const clean = () => {
+  return del("build");
+}
+
+exports.clean = clean;
+
+// posthtml
+
+const html = () => {
+  return gulp.src([
+    "source/*.html",
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+}
+
+exports.html = html;
+
+// Build
+
+const build = gulp.series(
+  clean,
+  styles,
+  copy,
+  scripts,
+  html
+);
+
+exports.build = build;
+
+// start
+
 exports.default = gulp.series(
-  styles, server, watcher
+  clean, styles, copy, scripts, html, server, watcher
 );
---
+++ source/js/script.js
@@ -0,0 +1,63 @@
+var pageHeader = document.querySelector(".page-header");
+var navMain = document.querySelector(".page-header__nav");
+var navToggle = document.querySelector(".page-header__toggler");
+var popupOverlay = document.querySelector(".pop-up-overlay");
+var popup = document.querySelector(".pop-up");
+var orderButtons = document.querySelectorAll(".product-card__to-cart, .product-promo__order");
+var map;
+
+function initMap() {
+  var device = {lat:59.938716, lng:30.323047};
+  var map = new google.maps.Map(
+      document.querySelector(".contacts__google-maps"), {center: device, zoom: 18})
+      var pin = {
+        url: "../img/map-pin.svg",
+        size: new google.maps.Size(67, 100),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(34, 100)
+      };
+
+      var marker = new google.maps.Marker({
+          position: device,
+          map: map,
+          icon: pin,
+          title: "Mishka"
+        });
+};
+
+pageHeader.classList.remove("page-header--nojs");
+
+navToggle.addEventListener("click", function() {
+  navToggle.classList.toggle("page-header__toggler--is-active");
+  if (navMain.classList.contains("page-header__nav--closed")) {
+    navMain.classList.remove("page-header__nav--closed");
+    navMain.classList.add("page-header__nav--opened");
+  } else {
+    navMain.classList.add("page-header__nav--closed");
+    navMain.classList.remove("page-header__nav--opened");
+  }
+});
+
+orderButtons.forEach(item => {
+  item.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.add("pop-up--active");
+    popupOverlay.classList.add("pop-up-overlay--active");
+  });
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode == 27) {
+    if (popup.classList.contains("pop-up--active") || popupOverlay.classlist.contains("pop-up-overlay--active")) {
+      popup.classList.remove("pop-up--active");
+      popupOverlay.classList.remove("pop-up-overlay--active");
+    }
+  }
+});
+
+popupOverlay.addEventListener("click", function(evt) {
+    if (popup.classList.contains("pop-up--active") || popupOverlay.classlist.contains("pop-up-overlay--active")) {
+      popup.classList.remove("pop-up--active");
+      popupOverlay.classList.remove("pop-up-overlay--active");
+    }
+});
